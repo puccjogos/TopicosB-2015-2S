df6d358a0404cfb517f1f576b1f0e52b04199c84
Enric Llagostera <enricllagostera@gmail.com>
2015-10-09 14:10:18 -0300

    graficos 2d e 3d (3d pre-aula)

---
+++ A09 Gráficos 2D/Assets/Jogador.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jogador : MonoBehaviour {
+
+	private Rigidbody2D _rb;
+	public Animator anim;
+	private float _inputX;
+	public float velocidade;
+	public Transform visual;
+
+	void Start () {
+		_rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	void Update () {
+		_inputX = Input.GetAxis ("Horizontal");
+		anim.SetFloat ("VelocidadeX", Mathf.Abs (_rb.velocity.x));
+		if (_inputX != 0) {
+			var escala = visual.localScale;
+			escala.x = Mathf.Sign(_inputX);
+			visual.localScale = escala;
+		}
+	}
+
+	void FixedUpdate() {
+		var velo = _rb.velocity;
+		velo.x = _inputX * velocidade;
+		_rb.velocity = velo;
+	}
+
+}
